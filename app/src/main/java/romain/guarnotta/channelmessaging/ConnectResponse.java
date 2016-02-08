5fa4d3412c6505c0ae3e9694810c7aa4ebe1d181
romain <guarnotta.romain@gmail.com>
2016-02-08 11:55:00 +0100

    UPDATE : Deserialize Gson data and save into a PrefsFile Add : ConnectResponse object to save the json

---
+++ app/src/main/java/romain/guarnotta/channelmessaging/ConnectResponse.java
@@ -0,0 +1,26 @@
+package romain.guarnotta.channelmessaging;
+
+/**
+ * Created by romain on 08/02/16.
+ */
+public class ConnectResponse {
+    private String response = "";
+    private String code = "";
+    private String accesstoken = "";
+
+    public String getResponse() {
+        return response;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getAccesstoken() {
+        return accesstoken;
+    }
+
+    ConnectResponse(){
+
+    }
+}
